ff73059892519d30f1e0944ec23e1360acebdc62
Jerel Miller <jermille@mines.edu>
2011-02-01 20:29:06 -0700

    fixed admin_controller.rb

--- project03/app/controllers/admin_controller.rb
+++ project03/app/controllers/admin_controller.rb
@@ -1,14 +1,14 @@
 class AdminController < ApplicationController
   def login
     if request.post?
-	  user = User.authenticate(params[:name], params[:password])
-	  if user
-	    session[:user_id] = user.id
-		redirect_to(:action => "index")
-	  else
-	    flash.now[:notice] = "Invalid user/password combination"
-	  end
-	end
+      user = User.authenticate(params[:name], params[:password])
+      if user
+        session[:user_id] = user.id
+	redirect_to(:action => "index")
+      else
+        flash.now[:notice] = "Invalid user/password combination"
+      end
+    end
   end
 
   def logout
