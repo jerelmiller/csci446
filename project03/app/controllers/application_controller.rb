3d86fc757e4c44da706d43e884e3e9fe7ab38489
Jerel Miller <jermille@mines.edu>
2011-02-01 17:53:30 -0700

    Spanish support

--- project03/app/controllers/application_controller.rb
+++ project03/app/controllers/application_controller.rb
@@ -2,9 +2,38 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  layout "store"
+  before_filter :authorize, :except => :login
+  before_filter :set_locale
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+protected
+  
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale]
+	I18n.locale = session[:locale] || I18n.default_locale
+	
+	locale_path = "#{LOCALES_DIRECTORY}#{I18n.locale}.yml"
+	
+	unless I18n.load_path.include? locale_path
+	  I18n.load_path << locale_path
+	  I18n.backend.send(:init_translations)
+	end
+  
+  rescue Exception => err
+    logger.error err
+	flash.now[:notice] = session[:locale] = I18n.default_locale
+  end
+end
+
+  def authorize
+    unless User.find_by_id(session[:user_id])
+	  flash[:notice] = "Please log in"
+	  redirect_to :controller => 'admin', :action => 'login'
+	end
+  end
 end
--- project03/app/controllers/store_controller.rb
+++ project03/app/controllers/store_controller.rb
@@ -1,12 +1,12 @@
 class StoreController < ApplicationController
+  before_filter :find_cart, :except => :empty_cart
+  
   def index
 	@products = Product.find_products_for_sale
-	@cart = find_cart
   end
   
   def add_to_cart
     product = Product.find(params[:id])
-	@cart = find_cart
 	@current_item = @cart.add_product(product)
 	respond_to do |format|
 	  format.js if request.xhr?
@@ -23,7 +23,6 @@ class StoreController < ApplicationController
   end
   
   def checkout
-    @cart = find_cart
 	if @cart.items.empty?
 	  redirect_to_index("Your cart is empty")
 	else
@@ -32,26 +31,30 @@ class StoreController < ApplicationController
   end
   
   def save_order
-    @cart = find_cart
 	@order = Order.new(params[:order])
 	@order.add_line_items_from_cart(@cart)
 	if @order.save
 	  session[:cart] = nil
-	  redirect_to_index("Thank you for your order")
+	  redirect_to_index(I18n.t('flash.thanks'))
 	else
 	  render :action => 'checkout'
 	end
   end
+ 
+protected
+  def authorize
+  end
   
-  private
   
-	def redirect_to_index(msg = nil)
-	  flash[:notice] = msg
-	  redirect_to :action => 'index'
-	end
+private
+  
+  def redirect_to_index(msg = nil)
+	flash[:notice] = msg
+	redirect_to :action => 'index'
+  end
 	
-	def find_cart
-	  session[:cart] ||= Cart.new
-	end
+  def find_cart
+	@cart = (session[:cart] ||= Cart.new)
+  end
 
 end
