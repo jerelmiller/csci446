d30be8b0f9df8eb3ac5de1c627a3be1a02471991
Jerel Miller <jermille@mines.edu>
2011-02-07 11:27:37 -0700

    Delete Old Files

--- project04/db/migrate/20110204225617_add_time_to_article.rb
+++
@@ -1,9 +0,0 @@
-class AddTimeToArticle < ActiveRecord::Migration
-  def self.up
-    add_column :articles, :time, :string
-  end
-
-  def self.down
-    remove_column :articles, :time
-  end
-end
--- project04/db/migrate/20110204234335_add_edits_to_article.rb
+++
@@ -1,9 +0,0 @@
-class AddEditsToArticle < ActiveRecord::Migration
-  def self.up
-    add_column :articles, :edits, :integer
-  end
-
-  def self.down
-    remove_column :articles, :edits
-  end
-end
