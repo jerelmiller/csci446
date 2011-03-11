959b4a3c6cceff6bd14a6cec315f8d994d3a99d5
Jerel Miller <jermille@mines.edu>
2011-03-11 15:50:47 -0700

    One more thing to do

--- project06/db/seeds.rb
+++ project06/db/seeds.rb
@@ -2,10 +2,30 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
-role_attributes = [ { :name => 'Admin' },
-                    { :name => 'Member' }
+#role_attributes = [ { :name => 'Admin' },
+                  #  { :name => 'Member' }
+                  #]
+                  
+user_attributes = [ { :firstname => 'Jack',
+                      :lastname => 'Admin',
+                      :username => 'administrator',
+                      :password => 'password',
+                      :password_confirmation => 'password',
+                      :role => Role.new(:name => 'Admin'),
+                      :email => 'jackadmin@gamez.com'
+                    },
+                    { :firstname => 'Diane',
+                      :lastname => 'Member',
+                      :username => 'member',
+                      :password => 'password',
+                      :password_confirmation => 'password',
+                      :role => Role.new(:name => 'Member'),
+                      :email => 'dianemember@gamez.com'
+                    }
                   ]
                   
-role_attributes.each do |attributes|
-  Role.create!(attributes)
+#
+
+user_attributes.each do |user|
+  User.create!(user)
 end
